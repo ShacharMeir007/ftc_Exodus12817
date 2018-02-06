07e62e6f0c8df1566e30aced3bbd50aa41beeaf4
shach <shachar.meir007@gmail.com>
2018-02-06 23:03:34 +0200

    2/6 ObjectTracker: added scanner info of test babushka and @TeleOp configure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectTracker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectTracker.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
@@ -17,7 +18,8 @@ import java.security.Policy;
 /**
  * Created by shach on 2/6/2018.
  */
-
+@TeleOp(name="Auto test", group="Linear Opmode")
+//@Disabled
 public class ObjectTracker extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
