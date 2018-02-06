917c640b059607d9902b48ab94dca59dd2c18140
shach <shachar.meir007@gmail.com>
2018-02-06 17:31:56 +0200

    2/6 ObjectTracker + grabber speed change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DroneDrive_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DroneDrive_Iterative.java
@@ -256,15 +256,15 @@ public class DroneDrive_Iterative extends OpMode {
 
 
 
-            if(gamepad2.right_trigger > 0.1)
+            if(gamepad2.left_trigger > 0.1)
             {
-                robot.B2Motor.setPower(-gamepad2.right_trigger*0.1);
+                robot.B2Motor.setPower(-gamepad2.right_trigger*0.2);
 
             }
             else {
-                if(gamepad2.left_trigger >0.1)
+                if(gamepad2.right_trigger>0.1)
                 {
-                    robot.B2Motor.setPower(gamepad2.left_trigger*0.3);
+                    robot.B2Motor.setPower(gamepad2.left_trigger*0.2);
 
                 }
                 else robot.B2Motor.setPower(0);
